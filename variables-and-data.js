fe176e905e170df7f9ab41b5eec0e590297e54d5
Diane Aguilar <daguilar59@cnm.edu>
2021-03-25 13:45:18 -0600

    Added variables-and-data.js, conditionals and functions.js, and objects.apis.js file

---
+++ variables-and-data.js
@@ -0,0 +1,31 @@
+/*
+ * Variables, Data Types, and Typing
+ */
+
+/*
+
+let wordData = "Word list";
+
+console.log(wordData);
+
+let subTotal = 100;
+let tax = subTotal * 0.07;
+let totalWithTax = tax + subTotal;
+
+ */
+
+let add = 1+2;
+let sub = 2-1;
+let mult = 2*4;
+let division = 4 / 2;
+let mod = 5 % 2;
+
+let string = "String 1 " + "String 2";
+
+console.log(string);
+
+
+
+
+
+
